b6b216f83dccbe77956c9d7760e3a26fb6d444b8
Praveen Selvaraj <sc19ps@leeds.ac.uk>
2022-11-08 00:01:31 +0000

    save to drive results file; dont run metrics

--- stylegan_K1/config.py
+++ stylegan_K1/config.py
@@ -10,7 +10,7 @@
 #----------------------------------------------------------------------------
 # Paths.
 
-result_dir = 'results'
+result_dir = '/content/drive/MyDrive/CFG/results'
 data_dir = 'datasets'
 cache_dir = 'cache'
 run_dir_ignore = ['results', 'datasets', 'cache']
