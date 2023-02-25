1e56b4545c55113b7c763298eb2dea0a0e5bb496
prat23k <jprathima@hotmail.com>
2023-02-25 23:18:02 +0000

    Create config.py

---
+++ stylegan2/config.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+#
+# This work is licensed under the Creative Commons Attribution-NonCommercial
+# 4.0 International License. To view a copy of this license, visit
+# http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to
+# Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+
+"""Global configuration."""
+
+#----------------------------------------------------------------------------
+# Paths.
+
+synthetic_data_size = 5
+model_dir = '/workspace/CFG.pkl'
+resume_dir = '/workspace/CFG.pkl'
+result_dir = '/workspace/stylegan_K1/results'
+generation_dir = '/workspace/stylegan_K1/synthetic_data'
+data_dir = '/workspace/stylegan_K1/datasets'
+cache_dir = 'cache'
+run_dir_ignore = ['results', 'datasets', 'cache']
+
+#----------------------------------------------------------------------------
